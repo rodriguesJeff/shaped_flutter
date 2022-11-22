41d8d398fe294a7cac98f313dfe2b8c95f07d4ab
rodriguesjeff <jefferson.rodrigues@seventh.com.br>
2022-11-21 21:10:55 -0300

    Finalized home page

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -22,7 +22,100 @@ class _HomePageState extends State<HomePage> {
       body: BlocBuilder<HomeCubit, HomeState>(builder: (context, state) {
         if (state is HomeInitial) _cubit.getPatientStatusModel();
         if (state is HomeLoaded) {
-          return Container();
+          return Column(
+            children: [
+              const SizedBox(height: 8.0),
+              for (final s in state.status) ...[
+                const SizedBox(height: 8.0),
+                Center(
+                  child: GestureDetector(
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (c) {
+                          return AlertDialog(
+                            title: Center(
+                              child: Text(
+                                'Resumo do Exame do(a)\n${s.paciente}',
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            content: SingleChildScrollView(
+                              child: Column(
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      const Text('Status:'),
+                                      Text(
+                                        s.status ?? '',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16.0,
+                                          color: s.status == 'Finalizado'
+                                              ? Colors.green
+                                              : Colors.blue,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      width: MediaQuery.of(context).size.width * .9,
+                      decoration: BoxDecoration(
+                        color: s.status == 'Finalizado'
+                            ? Colors.green
+                            : Colors.amber,
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text('Data:'),
+                                Text(
+                                  s.data ?? '',
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text('Nome:'),
+                                Text(
+                                  s.paciente ?? '',
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+              ]
+            ],
+          );
         }
         return const Center(
           child: CircularProgressIndicator(),
